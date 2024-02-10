7a872942e375988efe27b9f33a5f2fbbc66fc855
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-11 02:51:40 +0300

    Update install_c8.sh

--- install_c8.sh
+++ install_c8.sh
@@ -2,28 +2,28 @@
 cd $HOME
 
 # Remove the master.zip file
-rm master.zip
+rm ./master.zip
 
 # Download the pywikibot repository as a zip file from GitHub
 wget https://github.com/wikimedia/pywikibot/archive/refs/heads/master.zip
 
 # Unzip the downloaded master.zip file
-unzip master.zip
-rm master.zip
+unzip ./master.zip
+rm ./master.zip
 
 # Rename the extracted pywikibot-master directory to c8
-mv pywikibot-master c8
+mv ./pywikibot-master ./c8
 
 # Set the file permissions to 6770 for the c8 directory and its contents
-chmod -R 6770 c8
+chmod -R 6770 ./c8
 
 # Copy the 404-links.txt file from /data/project/himo/core8/ to the c8 directory
-cp /data/project/himo/core8/404-links.txt c8/404-links.txt
+cp /data/project/himo/core8/404-links.txt ./c8/404-links.txt
 
 # Set the file permissions to 6770 for the c8 directory and its contents
-chmod -R 6770 c8
+chmod -R 6770 ./c8
 
-rm -rf user-config.py
+rm -rf ./user-config.py
 
 # download https://github.com/MrIbrahem/user-config/blob/main/user-config.py to user-config.py
 wget https://github.com/MrIbrahem/user-config/blob/main/user-config.py
