eb5b61bdc78b3f41ac10537cf392277f6fae2937
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-11 02:22:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-wget https://github.com/MrIbrahem/user-config/blob/main/py_3114.sh
+wget https://raw.githubusercontent.com/MrIbrahem/user-config/main/py_3114.sh
 
 chmod -R 6770 py_3114.sh
 
 sh py_3114.sh
 
-wget https://github.com/MrIbrahem/user-config/blob/main/install_c8.sh
+wget https://raw.githubusercontent.com/MrIbrahem/user-config/main/install_c8.sh
 
 chmod -R 6770 install_c8.sh
 
