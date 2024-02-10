56fde28c56ac45bc2dcc6e703df15825473015e8
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-11 01:33:57 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+wget https://github.com/MrIbrahem/user-config/blob/main/py_3114.sh
+
+chmod -R 6770 py_3114.sh
+
+sh py_3114.sh
+
+wget https://github.com/MrIbrahem/user-config/blob/main/install_c8.sh
+
+chmod -R 6770 install_c8.sh
+
+sh install_c8.sh
