640e68402b4250378e2abe4b48ea89db3887a942
ibrahem <ibrahem.al-radaei@outlook.com>
2024-07-06 00:11:09 +0300

    Update py_311.sh

--- py_311.sh
+++ py_311.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python3 -m pip freeze > last_req.txt
+$HOME/local/bin/python3 -m pip freeze > last_req.txt
 
 # activate it
 source pyvenv/bin/activate
 
 # install some concrete packages
-pyvenv/bin/pip install -U requests packaging wikitextparser python-dateutil certifi gorilla gorilla-cli
-pyvenv/bin/pip install -r requirements_new.txt
-pyvenv/bin/pip install -r c8/requirements.txt
+pip install -U requests packaging wikitextparser python-dateutil certifi gorilla gorilla-cli
+pip install -r requirements_new.txt
+pip install -r c8/requirements.txt
 
 python3 -m pip list --outdated
 python3 -m pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./pyvenv/bin/pip311 install -U
